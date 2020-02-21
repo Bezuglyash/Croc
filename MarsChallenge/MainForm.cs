3a4a6b0cbde2ac4db3fb7af245c4763c8614ba9a
Maxim <max.ronald9@gmail.com>
2020-02-21 21:31:44 +0300

    Cource

---
+++ MarsChallenge/MainForm.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace MarsChallenge
+{
+    public partial class MainForm : Form
+    {
+        private const int max = 100;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<Pair> list = new List<Pair>();
+
+            // Перебор всех пар чисел от 2 до 99
+            for (int a = 2; a < max; a++)
+            {
+                for (int b = a + 1; b < max; b++)
+                {
+                    if (a + b < max)
+                    {
+                        list.Add(new Pair(a, b));
+                    }
+                }
+            }
+            // Группируем по суммам
+            foreach(var group in list.GroupBy(a => a.Sum).ToList())
+            {
+                if (group.Count() == 1)
+                {
+                    list.Remove(list.First(a => a.Sum == group.Key));
+                }
+            }
+
+            foreach (var group in list.GroupBy(a => a.Mul).ToList())
+            {
+                if (group.Count() == 1)
+                {
+                    list.Remove(list.First(a => a.Mul == group.Key));
+                }
+            }
+
+            foreach (var group in list.GroupBy(a => a.Sum).ToList())
+            {
+                if (group.Count() > 1)
+                {
+                    foreach(var item in group)
+                    {
+                        list.Remove(item);
+                    }
+                }
+            }
+
+            foreach (var group in list.GroupBy(a => a.Mul).ToList())
+            {
+                if (group.Count() > 1)
+                {
+                    foreach (var item in group)
+                    {
+                        list.Remove(item);
+                    }
+                }
+            }
+            MessageBox.Show($"Пара: {list[0].A} и {list[0].B}");
+        }
+    }
+}
---
+++ MarsChallenge/Pair.cs
@@ -0,0 +1,20 @@
+﻿
+
+namespace MarsChallenge
+{
+    class Pair
+    {
+        readonly public int A;
+        readonly public int B;
+        readonly public int Sum;
+        readonly public int Mul;
+
+        public Pair(int a, int b)
+        {
+            A = a;
+            B = b;
+            Sum = a + b;
+            Mul = a * b;
+        }
+    }
+}
