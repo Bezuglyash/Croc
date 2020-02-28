761efba66d1932217dae623615a6ddb2d25411bf
Maxim <max.ronald9@gmail.com>
2020-02-28 21:03:00 +0300

    Add update code

--- MarsChallenge/MainForm.Designer.cs
+++ MarsChallenge/MainForm.Designer.cs
@@ -28,35 +28,114 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.tool = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton3 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton4 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton5 = new System.Windows.Forms.ToolStripButton();
+            this.toolStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // button1
+            // toolStrip1
             // 
-            this.button1.Font = new System.Drawing.Font("Times New Roman", 28F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(10, 7);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(87, 93);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "0";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tool,
+            this.toolStripButton2,
+            this.toolStripButton3,
+            this.toolStripButton4,
+            this.toolStripButton5});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(360, 25);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            this.toolStrip1.UseWaitCursor = true;
+            // 
+            // tool
+            // 
+            this.tool.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.tool.Image = ((System.Drawing.Image)(resources.GetObject("tool.Image")));
+            this.tool.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tool.Name = "tool";
+            this.tool.Size = new System.Drawing.Size(23, 22);
+            this.tool.Text = "1";
+            this.tool.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton2.Text = "2";
+            this.toolStripButton2.Click += new System.EventHandler(this.toolStripButton2_Click);
+            // 
+            // toolStripButton3
+            // 
+            this.toolStripButton3.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton3.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton3.Image")));
+            this.toolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton3.Name = "toolStripButton3";
+            this.toolStripButton3.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton3.Text = "3";
+            this.toolStripButton3.Click += new System.EventHandler(this.toolStripButton3_Click);
+            // 
+            // toolStripButton4
+            // 
+            this.toolStripButton4.AccessibleName = "4";
+            this.toolStripButton4.AccessibleRole = System.Windows.Forms.AccessibleRole.SplitButton;
+            this.toolStripButton4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.toolStripButton4.Checked = true;
+            this.toolStripButton4.CheckState = System.Windows.Forms.CheckState.Indeterminate;
+            this.toolStripButton4.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton4.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton4.Name = "toolStripButton4";
+            this.toolStripButton4.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.toolStripButton4.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton4.Text = "4";
+            this.toolStripButton4.TextImageRelation = System.Windows.Forms.TextImageRelation.Overlay;
+            this.toolStripButton4.Click += new System.EventHandler(this.toolStripButton4_Click);
+            // 
+            // toolStripButton5
+            // 
+            this.toolStripButton5.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton5.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton5.Image")));
+            this.toolStripButton5.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton5.Name = "toolStripButton5";
+            this.toolStripButton5.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton5.Text = "0";
+            this.toolStripButton5.Click += new System.EventHandler(this.toolStripButton5_Click);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(933, 450);
-            this.Controls.Add(this.button1);
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(360, 450);
+            this.Controls.Add(this.toolStrip1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "MainForm";
             this.Text = "Задача МАРС";
+            this.UseWaitCursor = true;
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton tool;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+        private System.Windows.Forms.ToolStripButton toolStripButton3;
+        private System.Windows.Forms.ToolStripButton toolStripButton4;
+        private System.Windows.Forms.ToolStripButton toolStripButton5;
     }
 }
--- MarsChallenge/MainForm.cs
+++ MarsChallenge/MainForm.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Windows.Forms;
 
@@ -8,6 +9,7 @@ namespace MarsChallenge
     public partial class MainForm : Form
     {
         private const int max = 100;
+        private List<Pair> list = new List<Pair>();
 
         public MainForm()
         {
@@ -16,9 +18,12 @@ namespace MarsChallenge
 
         private void button1_Click(object sender, EventArgs e)
         {
-            List<Pair> list = new List<Pair>();
+            // Группируем по суммам
+            MessageBox.Show($"Пара: {list[0].A} и {list[0].B}");
+        }
 
-            // Перебор всех пар чисел от 2 до 99
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
             for (int a = 2; a < max; a++)
             {
                 for (int b = a + 1; b < max; b++)
@@ -26,37 +31,60 @@ namespace MarsChallenge
                     if (a + b < max)
                     {
                         list.Add(new Pair(a, b));
+                        SetColor(a, b, Color.Blue);
                     }
                 }
             }
-            // Группируем по суммам
-            foreach(var group in list.GroupBy(a => a.Sum).ToList())
+        }
+
+        private string getName(int a, int b)
+        {
+            return $"cell{a:000}{b:000}";
+        }
+
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+            foreach (var group in list.GroupBy(a => a.Sum).ToList())
             {
                 if (group.Count() == 1)
                 {
-                    list.Remove(list.First(a => a.Sum == group.Key));
+                    var pair = list.First(a => a.Sum == group.Key);
+                    list.Remove(pair);
+                    SetColor(pair.A, pair.B, Color.Blue);
                 }
             }
+        }
 
-            foreach (var group in list.GroupBy(a => a.Mul).ToList())
+        private void toolStripButton3_Click(object sender, EventArgs e)
+        {
+            foreach (IGrouping<int, Pair> group in list.GroupBy(a => a.Mul).ToList())
             {
                 if (group.Count() == 1)
                 {
-                    list.Remove(list.First(a => a.Mul == group.Key));
+                    var pair = list.First(a => a.Mul == group.Key);
+                    list.Remove(pair);
+                    SetColor(pair.A, pair.B, Color.Blue);
                 }
             }
+        }
 
+        private void toolStripButton4_Click(object sender, EventArgs e)
+        {
             foreach (var group in list.GroupBy(a => a.Sum).ToList())
             {
                 if (group.Count() > 1)
                 {
-                    foreach(var item in group)
+                    foreach (var item in group)
                     {
                         list.Remove(item);
+                        SetColor(item.A, item.B, Color.Blue);
                     }
                 }
             }
+        }
 
+        private void toolStripButton5_Click(object sender, EventArgs e)
+        {
             foreach (var group in list.GroupBy(a => a.Mul).ToList())
             {
                 if (group.Count() > 1)
@@ -64,10 +92,33 @@ namespace MarsChallenge
                     foreach (var item in group)
                     {
                         list.Remove(item);
+                        SetColor(item.A, item.B, Color.Yellow);
                     }
                 }
             }
-            MessageBox.Show($"Пара: {list[0].A} и {list[0].B}");
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            for (int a = 2; a < max; a++)
+            {
+                for (int b = 2; b < max; b++)
+                {
+                    var lbl = new Label();
+                    lbl.Size = new System.Drawing.Size(4, 4);
+                    lbl.Location = new System.Drawing.Point(a * 4, b * 4 + tool.Height);
+                    lbl.BackColor = Color.Yellow;
+                    lbl.Name = getName(a, b);
+                    Controls.Add(lbl);
+                }
+            }
+        }
+
+        private void SetColor(int x, int y, Color color)
+        {
+            string name = getName(x, y);
+            Label lbl = (Label)Controls[name];
+            lbl.BackColor = color;
         }
     }
 }
