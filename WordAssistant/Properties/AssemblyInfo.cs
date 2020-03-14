99d281275334d58b1d423fa6b331e588683a0283
Max <max.ronald9@gmail.com>
2020-03-14 23:40:29 +0300

    Add Word Assistant project

---
+++ WordAssistant/Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Security;
+
+// Управление общими сведениями о сборке осуществляется посредством следующего 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// общие сведения об этой сборке.
+[assembly: AssemblyTitle("WordAssistant")]
+[assembly: AssemblyDescription("Надстройка для Microsoft Word для переноса свойств документов между открытыми документами")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("ЗАО КРОК инкорпорейтед")]
+[assembly: AssemblyProduct("Учебный курс 'Разработка приложений на языке С#'")]
+[assembly: AssemblyCopyright("Copyright © CROC incorporated 2020")]
+[assembly: AssemblyTrademark("BrainZ")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False в параметре ComVisible делает типы в этой сборке невидимыми 
+// для компонентов COM.  Если необходимо обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("eecfc598-a0fe-4bc3-8663-8b06558f21dc")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
